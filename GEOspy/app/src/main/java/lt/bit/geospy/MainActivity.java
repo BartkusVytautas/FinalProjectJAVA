043f08162f338e7fea64c330fa8604dce6116867
Vytautas <vytautas.bartkus91@gmail.com>
2019-10-02 14:16:39 +0300

    test

--- GEOspy/app/src/main/java/lt/bit/geospy/MainActivity.java
+++ GEOspy/app/src/main/java/lt/bit/geospy/MainActivity.java
@@ -2,23 +2,59 @@ package lt.bit.geospy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView title = findViewById(R.id.titlename);
+    TextView loading = findViewById(R.id.loading);
+    WebView webView =findViewById(R.id.web);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_loading_screen);
+
         //Todo generate API key at the first launch
 
-        WebView webView = findViewById(R.id.web);
+
+        Animation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(150);
+        anim.setStartOffset(20);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        title.startAnimation(anim);
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///android_asset/map.html");
-    }
 
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    wait(5000);
+                    title.setVisibility(View.GONE);
+                    loading.setVisibility(View.GONE);
+                    webView.setVisibility(View.VISIBLE);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
 
 }
